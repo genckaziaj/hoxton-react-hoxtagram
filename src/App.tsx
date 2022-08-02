1bc20306d332a66669552bc82a6d78ae1b95567a
Genci23 <genckaziaj@yahoo.com>
2022-08-02 03:04:57 +0200

    Images and Likes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [images, setImages] = useState([]);
+
+  function addLikes(id: number) {
+    let imagesCopy = structuredClone(images);
+
+    const match = imagesCopy.find((image) => image.id === id);
+    match.likes++;
+
+    fetch(`http://localhost:3001/images/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(match),
+    });
+
+    setImages(imagesCopy);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3001/images")
+      .then((resp) => resp.json())
+      .then((imagesFromServer) => {
+        setImages(imagesFromServer);
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -10,20 +35,28 @@ function App() {
       {/* <!-- image cards --> */}
       <section className="image-container">
         {/* <!-- This is the HTML for each card. Use the following HTML as reference to build your React components --> */}
-
-        <article className="image-card">
-          <h2 className="title">Title of image goes here</h2>
-          <img src="./assets/image-placeholder.jpg" className="image" />
-          <div className="likes-section">
-            <span className="likes">0 likes</span>
-            <button className="like-button">♥</button>
-          </div>
-          <ul className="comments">
-            <li>Get rid of these comments</li>
-            <li>And replace them with the real ones</li>
-            <li>From the server</li>
-          </ul>
-        </article>
+        {images.map((image) => (
+          <article className="image-card" key={image.id}>
+            <h2 className="title">{image.title}</h2>
+            <img src={image.image} className="image" />
+            <div className="likes-section">
+              <span className="likes">{image.likes} likes</span>
+              <button
+                className="like-button"
+                onClick={() => {
+                  addLikes(image.id);
+                }}
+              >
+                ♥
+              </button>
+            </div>
+            <ul className="comments">
+              <li>Get rid of these comments</li>
+              <li>And replace them with the real ones</li>
+              <li>From the server</li>
+            </ul>
+          </article>
+        ))}
       </section>
     </div>
   );
